f5ce4981bd2445c0b442886bf0e9b4536497edb9
Abby Johnson <abby.johnson@utexas.edu>
2022-02-18 23:39:51 -0600

    Rproj with incarcerated data

--- R/output/table_incarcerations_by_racegender.R
+++ R/output/table_incarcerations_by_racegender.R
@@ -5,7 +5,7 @@
 # https://cran.r-project.org/web/packages/kableExtra/vignettes/awesome_table_in_pdf.pdf
 
 read_csv(here("data/NLSY97_clean.csv")) %>%
-  
+  filter(total_time_incarcerated>0)%>%
   # summarize arrests by race and gender
   group_by(race, gender) %>%
   summarize(total_time_incarcerated = mean(total_time_incarcerated)) %>%
